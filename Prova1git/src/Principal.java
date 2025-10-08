9ab0cbbc61337cb64755994e000b2834d76ef337
Miquel <mpetrus@iesjoanramis.org>
2025-10-08 14:48:07 +0200

    Tercera línia

--- Prova1git/src/Principal.java
+++ Prova1git/src/Principal.java
@@ -3,6 +3,7 @@ public class Principal {
 	public static void main (String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Segon missatge");
+		System.out.println("Tercer missatge");
 		
 	}
 }
